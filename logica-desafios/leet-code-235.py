85c183d5ee1708c9559a7f1d2228b8a410456994
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-02-25 22:41:35 -0300

    algoritmos

---
+++ logica-desafios/leet-code-235.py
@@ -0,0 +1,28 @@
+class TreeNode:
+    def __init__(self, val, left=None, right=None)
+        self.val = val
+        self.left = left
+        self.right = right
+
+def lowestCommonAncestor(root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+    if root == p.val or root == q.val:
+        return root
+    
+    if (p.val > root.val and q.val < root.val) or (p.val < root.val and q.val > root.val):
+
+# Left view of the binary tree
+n3 = TreeNode(3)
+n5 = TreeNode(5)
+n4 = TreeNode(4, n3, n5)
+n0 = TreeNode(0)
+n2 = TreeNode(2, n0, n4)
+
+# Right view of the binary tree
+n7 = TreeNode(7)
+n9 = TreeNode(9)
+n8 = TreeNode(8, n7, n9)
+
+# root
+n6 = TreeNode(6, n2, n8)
+
+print(lowestCommonAncestor(n6, n2, n8). val)

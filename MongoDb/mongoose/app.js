6adab021c824ded0e083cae6348c9c63b7d2257b
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-03-04 15:21:28 -0300

    ...

--- MongoDb/mongoose/app.js
+++ MongoDb/mongoose/app.js
@@ -3,21 +3,8 @@ const app = express()
 const port = 3000
 const mongoose = require('mongoose')
 const path = require('path')
-
 const linkRoute = require('./routes/linkRoute')
-const Link = require('./models/Link')
 
-// let link = new Link({
-//     title: "chagas",
-//     description: "Link para o X",
-//     url: "https://twitter.com/Chagao",
-//     click: 0,
-// })
-// link.save().then(doc => {
-//     console.log(doc)
-// }).catch(err => {console.log(err)})
-
-// conexao com o mongo pelo link
 mongoose.connect('mongodb://localhost/newlinks', { useUnifiedTopology: true, useNewUrlParser: true });
 
 let db = mongoose.connection;

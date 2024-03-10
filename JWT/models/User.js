770e3d8857802e5b862ef52d4405f360a97fda9a
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-03-10 17:16:47 -0300

    feat: add rotas

--- JWT/models/User.js
+++ JWT/models/User.js
@@ -3,6 +3,7 @@ const userSchema = mongoose.Schema({
     name: {typr: String, required: true, minlength: 3, maxLength: 50},
     email: {typr: String, required: true, minlength: 3, maxLength: 100},
     passwort: {typr: String, required: true, minlength: 6, maxLength: 10},
+    admin: {type: boolean, default: Date.now},
     createdAt: {type: Date, default: Date.now}
 })
 
--- JWT/routes/adminRouter.js
+++ JWT/routes/adminRouter.js
@@ -4,7 +4,14 @@ const router = express.Router();
 const auth = require('../controllers/authController')
 
 router.get('/', auth, (req, res) => {
-    res.send('Esse dado so deve ser visto pelo admin')
+
+    if (req.secure.admin) {
+        res.send('Esse dado so deve ser visto pelo admin')
+
+    } else {
+        res.status(401).send('Acess Denied');
+    }
+    
 })
 
 module.exports = router

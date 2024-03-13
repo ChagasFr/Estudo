4eb0b7e2a405d6a605cd18d907129d78fa6e1244
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-03-13 17:25:15 -0300

    feat: user

--- Socket/Chat/public/chat.js
+++ Socket/Chat/public/chat.js
@@ -1,6 +1,6 @@
 const socket = io("http://localhost:3000")
 
-let user = ''
+let user = null
 
 socket.on('update_messages', (messages) => {
     updateMessagesOnScrean(messages)
@@ -10,7 +10,7 @@ function updateMessagesOnScrean(messages) {
 
     let list_messages = '<ul>'
     messages.forEach(message => {
-        list_messages += `<li>${message}</li>`
+        list_messages += `<li>${message.user}: ${message.msg}</li>`
     })
     list_messages += `</ul>`
 
@@ -20,16 +20,23 @@ function updateMessagesOnScrean(messages) {
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.querySelector('#message_form');
     form.addEventListener('submit', (e) => {
+
+        if(!user) {
+            alert('defina um usuario');
+            return;
+        }
+
         e.preventDefault();
         const message = document.forms['message_form_name'] ['msg'].value;
         document.forms['message_form_name'] ['msg'].value = ''
-        socket.emit('new_message', { msg: message })
+        socket.emit('new_message', { user:user, msg: message })
         
     })
 
     const userForm = document.querySelector('#user_form_name');
     userForm.addEventListener('submit', (e) => {
         e.preventDefault();
-        const message = document.forms['user_form_name'] ['user'].value;
+        user = document.forms['user_form_name'] ['user'].value;
+        userForm.parentNode.removeChild(userForm)
     })
 })

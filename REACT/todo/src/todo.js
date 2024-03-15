7299fca3d44e6f47565f44db57a8ec098c99ecbe
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-03-15 17:33:36 -0300

    feat: adicionando itens

--- REACT/todo/src/todo.js
+++ REACT/todo/src/todo.js
@@ -1,7 +1,31 @@
 import React from "react";
 
 function Todo() {
-    return (<div className="container"><h1>Todo</h1></div>)
+    const [text, setText] = useState("");
+    const [items, setItems] = useState([]);
+    
+    function handleChange(event) {
+        let t = event.target.value;
+        setText(t);
+    }
+
+    function addItem (event) {
+        event.preventDefault();
+        setItems([...items, text])
+        setText("");
+    }
+
+    return (<div className="container">
+        <h1>Todo</h1>
+        <form>
+            <input onChange={handleChange} type="text" value={text}></input>
+            <button onClick={addItem}>Add</button>
+        </form>
+
+        <ul>
+            <li>Item</li>
+        </ul>
+    </div>)
 }
 
 export default Todo

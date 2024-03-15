95ae3ee2469d895ae62c29626b204a79d902ceed
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-03-15 19:07:41 -0300

    feat: refatorando

--- REACT/todo/src/todo.js
+++ REACT/todo/src/todo.js
@@ -1,41 +1,22 @@
 import React from "react";
-import './Todo.css'
 import List from "./List";
+import TodoForm from "./TodoForm";
+import './Todo.css'
 
 function Todo() {
-    const [text, setText] = useState("");
+    
     const [items, setItems] = useState([]);
+
+    function onAddItem(item) {
+        setItems([...item, item])
+    }
 
     return (<div className="container">
         <h1>Todo</h1>
-        <TodoForm></TodoForm>
+        <TodoForm onAddItem={onAddItem}></TodoForm>
         <List items={items}></List>
 
     </div>)
-}
-
-function TodoForm(props) {
-
-    function handleChange(event) {
-        let t = event.target.value;
-        setText(t);
-    }
-
-    function addItem (event) {
-        event.preventDefault();
-
-        if(text) {
-            setItems([...items, text])
-            setText("");
-        }
-    }
-
-    return (
-        <form>
-        <input onChange={handleChange} type="text" value={text}></input>
-        <button onClick={addItem}>Add</button>
-        </form>
-    )
 }
 
 

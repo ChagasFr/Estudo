ac3d56e60a3e273d06987e06a7f87b53394af5db
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-03-18 16:11:29 -0300

    fix: separando components

--- REACT/todo/src/components/List.js
+++ REACT/todo/src/components/List.js
@@ -1,17 +1,9 @@
 import React from "react"
-import Card from "./Card"
-
-function DoneImg(props) {
-    if(props.done) {
-       return <img alt="done" src="../public/assets/on.png"></img>
-    } else {
-        return (<img alt="undone" src="../public/assets/off.jpg"></img>)
-    }
-}
+import ListItem from "./ListItem";
 
 function List(props) {
     return (<ul>
-        {props.items.map(item =>)} 
+        {props.items.map(item => <ListItem></ListItem>)} 
     </ul>)
 }
 

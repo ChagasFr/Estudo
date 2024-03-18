c505f09a2fad595b16ce161b4fd4f5efef23b252
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-03-18 14:48:14 -0300

    feat: desenvolvimento component card

--- REACT/todo/src/components/List.js
+++ REACT/todo/src/components/List.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Card from "./Card"
 
 function DoneImg(props) {
     if(props.done) {
@@ -10,11 +11,14 @@ function DoneImg(props) {
 
 function List(props) {
     return (<ul>
-        {props.items.map(item => <li className={item.done ? "done" : ""} key={item.id}>
-            {item.text}
-            <button onClick={() =>{ props.onDone(item) }}><DoneImg done={item.done}></DoneImg></button>
-            <button onClick={() => {props.onItemDeleted(item)}}><img alt="delete" src="../public/assets/bin.jpg"></img></button>
-        
+        {props.items.map(item => <li className={item.done ? "done item" : "item"} key={item.id}>
+            <Card>
+                {item.text}
+                <div>
+                    <button onClick={() =>{ props.onDone(item) }}><DoneImg done={item.done}></DoneImg></button>
+                    <button onClick={() => {props.onItemDeleted(item)}}><img alt="delete" src="../public/assets/bin.jpg"></img></button>
+                </div>
+            </Card>
         </li>)} 
     </ul>)
 }

7fb27c4c74dc2686674d90f42298dc70ec6299e0
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-03-20 18:13:41 -0300

    my first commit

---
+++ Mural/index.js
@@ -0,0 +1,21 @@
+const PORT = 3000;
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+let posts = [
+    {
+        id: "kaldjhalkjhdslkjas",
+        title: "Teste do mural",
+        description: "Descricao"
+    },
+]
+
+app.get("./all", (req, res) => {});
+
+app.post("./new", (req, res) => {});
+
+app.listen(PORT, () => {
+    console.log("Server running on port", PORT)
+})

b1c72c67fb451a4859e951092fda098170afbfd3
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-03-21 18:55:47 -0300

    fea: api

--- Mural/index.js
+++ Mural/index.js
@@ -1,22 +1,12 @@
 const PORT = 3000;
-const express = require('express');
 const bodyParser = require('body-parser');
-const posts = require('./model/posts')
+const posts = require('./model/posts');
+const path = require("path");
+
 
 const app = express();
 
-app.get("./all", (req, res) => {
-    res.json(JSON.stringify(posts.getAll()))
-});
-
-app.post("./new", bodyParser.json(), (req, res) => {
-    let title = req.body.title;
-    let description = req.body.description;
-
-    posts.newPosts(title, description)
-
-    res.send("Post adicinado");
-});
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => {
     console.log("Server running on port", PORT)

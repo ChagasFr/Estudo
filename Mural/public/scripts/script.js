447358f55d80721a6686d8169857f78e9f5125c9
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-03-22 20:02:28 -0300

    feat: desenvolvimento posts mural

--- Mural/public/scripts/script.js
+++ Mural/public/scripts/script.js
@@ -10,7 +10,16 @@ function updatePosts() {
 
         let posts = JSON.parse(json);
         posts.forEach((post) => {
-            let postElement = ``
+            let postElement = `
+                <div id=${post.id} class="card mb-4">
+                    <div class="card-header">
+                        <h5 class=${post.title}>Title</h5>
+                    </div>
+                    <div class="card-body">
+                        <div class="card-text">${post.descricao}</div>
+                    </div>
+                </div> `
+            postElements += postElement;
         })
     })
 }

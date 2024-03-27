d03c7c95f291d63640b3334f169bda3ddbfd2be3
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-03-26 22:39:47 -0300

    feat: variaveis

--- Template-engine/index.js
+++ Template-engine/index.js
@@ -3,15 +3,15 @@ const fs = require('fs');
 const path = require('path')
 const app = express();
 
-function render(data, obj) {
-
+let user = {
+    name: "Felipe Chagas"
 }
 
 app.set("views", path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render('user')
+    res.render('user', {name: "Felipe Chagas", phone: "(11) 321-302"} )
 })
 
 app.listen(3000, () => {

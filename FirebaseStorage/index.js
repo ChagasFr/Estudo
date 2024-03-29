292bb657dec44f266d74d423255de9294e2cc95f
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-03-28 22:45:24 -0300

    feat: importacao firebase

---
+++ FirebaseStorage/index.js
@@ -0,0 +1,9 @@
+  const firebaseConfig = {
+    apiKey: "AIzaSyBLsH7u58BBYLbyDzp3AEjAHXGXPHGqaps",
+    authDomain: "bitcent-desc-f09d7.firebaseapp.com",
+    projectId: "bitcent-desc-f09d7",
+    storageBucket: "bitcent-desc-f09d7.appspot.com",
+    messagingSenderId: "221920479446",
+    appId: "1:221920479446:web:3799ead21f790121d32177"
+  };
+

23e527bd64a824e42b5503b17f849b2c880fb9fa
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-04-09 16:48:35 -0300

    feat: listando reducers

--- REACT/todo/src/reducers/listReducer.js
+++ REACT/todo/src/reducers/listReducer.js
@@ -3,8 +3,13 @@ export default (state = [], action) => {
         case "ADD_ITEM":
             return [ ...state, action.payload ]
         case "DELETE_ITEM":
-            return state.filter(item => item !=)
+            return state.filter(item => item.id != action.payload)
         case "CHANGE_DONE":
+            return state.map(item => {
+                if(item.id == action.payload) {
+                    item.done = !item.done
+                }
+            })
         default:
             return state;
     }

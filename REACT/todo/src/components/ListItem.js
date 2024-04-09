962957b8063eb54472ca8845234fc3a8ffac14ed
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-04-09 18:09:07 -0300

    fix: ajustando codigo

--- REACT/todo/src/components/ListItem.js
+++ REACT/todo/src/components/ListItem.js
@@ -1,5 +1,7 @@
 import React from "react"
 import Card from "./Card"
+import { useDispatch } from 'react-redux'
+import { deleteItem, changeDone } from '../actions'
 
 function DoneImg(props) {
     if(props.done) {
@@ -10,13 +12,14 @@ function DoneImg(props) {
 }
 
 function ListItem(props) {
+    const dispatch = useDispatch()
     return (
         <li key={props.item.id}>
             <Card className={props.item.done ? "done item" : "item"}>
                 {props.item.text}
                 <div>
-                    <button onClick={() =>{ props.onDone(props.item) }}><DoneImg done={props.item.done}></DoneImg></button>
-                    <button onClick={() => {props.onItemDeleted(props.item)}}><img alt="delete" src="../public/assets/bin.jpg"></img></button>
+                    <button onClick={() =>{ dispatch(changeDone(props.item.id)) }}><DoneImg done={props.item.done}></DoneImg></button>
+                    <button onClick={() => { dispatch(deleteItem(props.item.id)) }}><img alt="delete" src="../public/assets/bin.jpg"></img></button>
                 </div>
             </Card>
         </li>)

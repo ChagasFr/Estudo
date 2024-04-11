593e678917693da5fde8d0b7baee556300e2b8fc
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-04-11 13:45:11 -0300

    feat: data

---
+++ REACT/Rout/rout/src/Data.js
@@ -0,0 +1,7 @@
+const aulas = [
+    { id: 0, title: "Aula Inicial", desc: "Aula de apresentacao" },
+    { id: 1, title: "Como configurar", desc: "Configurando" },
+    { id: 2, title: "Hello Word", desc: "fazendo primeiro Hello Word" }
+]
+
+export default aulas;

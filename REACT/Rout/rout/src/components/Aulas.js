9b845c29517630ac2f2e044bb5b8adb45eb7a09c
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-04-12 18:50:24 -0300

    feat: aulas component

--- REACT/Rout/rout/src/components/Aulas.js
+++ REACT/Rout/rout/src/components/Aulas.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import data from '../Data'
-import { Link } from "react-router-dom";
+import { Link, useRouteMatch } from "react-router-dom";
 
 export default function Aulas(props) {
+    const match = useRouteMatch() 
+        useEffect(() => {
+            console.log(match)
+        })
+    
+
     return (
         <div className="page">
             <h1>Aulas</h1>
             <ul className="aulas">
                 {data.map (aula =>
-                    <li key = { aula.id }><Link className="link">{ aula.title }</Link></li> 
+                // link dinamico
+                    <li key = { aula.id }><Link to={ `${ match.url }${ aula.id }` } className="link">{ aula.title }</Link></li> 
                 )}
             </ul>
         </div>

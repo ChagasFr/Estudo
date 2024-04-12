d917aada0fa0e61f793160853e30f4253471b223
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-04-12 16:09:31 -0300

    feat: nav

--- REACT/Rout/rout/src/components/Nav.js
+++ REACT/Rout/rout/src/components/Nav.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Nav(props) {
     return (<nav className="nav">
         <h1>Chagas</h1>
-        <ul className='nav-links'></ul>
+        <ul className='nav-links'>
+            <li><Link to="/aulas"></Link>aulas</li>
+            <li><Link to="/sobre"></Link>sobre</li>
+        </ul>
+
     </nav>)
 }

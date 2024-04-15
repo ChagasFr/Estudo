e9fc2c8a14f9cc5c044f77a4ae118ce010f4f5b0
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-04-15 17:11:56 -0300

    feat: login reducer

---
+++ REACT/Rout/rout/src/reducers/loginReducers.js
@@ -0,0 +1,10 @@
+export default function loginReducers(state = false, action) {
+    switch (action.type) {
+        case 'LOGIN':
+            return true;
+        case 'LOGOUT':
+            return false    
+        default:
+            return state
+    }
+}

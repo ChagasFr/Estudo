984f61fa2560a112bde122400defa270b5cdffb1
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-04-15 17:43:17 -0300

    feat: buttons

--- REACT/Rout/rout/src/components/authButton.js
+++ REACT/Rout/rout/src/components/authButton.js
@@ -5,4 +5,15 @@ import { useSelector, useDispatch } from 'react-redux'
 export default function Auth(props) {
     const login = useSelector (state => state)
     const dispatch = useDispatch()
+
+    return (login ? 
+        <button
+            onClick={() => { dispatch(logoutAction()) }}>
+            logout
+        </button>: 
+        <button
+            onClick={() => { dispatch(loginAction) }}>
+            login
+        </button>
+    )
 }

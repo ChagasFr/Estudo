9e3b09be7373590d2f0b98d775e585320b9c850a
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-04-15 11:12:59 -0300

    feat: pegando params

--- REACT/Rout/rout/src/components/Aula.js
+++ REACT/Rout/rout/src/components/Aula.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from 'react-router-dom'
 
 export default function Aula(props) {
+    const params = useParams()
+
+    const [aula, setAula] = useState ({})
+
+    useEffect(() => {
+
+    }, [params])
     return (
         <div className="page">
             <h1>Title</h1>

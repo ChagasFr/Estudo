b589e76e55eae033cefa8871e9c37f4ed31ea29d
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-04-16 16:13:36 -0300

    feat: desenvolvendo projeto

--- REACT/Videos/index.js
+++ REACT/Videos/index.js
@@ -1,6 +1,20 @@
 const express = require ('express')
-
 const app = express();
+const path = require("path");
+
+const videos = [
+    "k312jh1kj23h",
+    "lk76567kl5j6",
+    "43hj2g4j23hj",
+    "sad904j23kwe",
+    "dsa67SDL2345",
+]
+
+app.get('/api/videos', (req, res) => {
+    res.send(videos);
+})
+
+app.use(express.static(path.join(__dirname, 'front/build')))
 
 app.listen(3000, () => {
     console.log('running')

1692e3005c6da586dabe6e3953f63cffefcf159e
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-04-16 14:12:36 -0300

    feat: nav component

--- REACT/Rout/rout/src/components/Nav.js
+++ REACT/Rout/rout/src/components/Nav.js
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import AuthButton from '../components/authButton'
 
 import { useSelector } from 'react-redux-dom'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 
 export default function Nav(props) {
     const login = useSelector(state => state)
 
+    const history = useHistory();
+
+    useEffect(() => {
+        if(login) {
+            history.push('/')
+        }
+    }, [login])
+
     return (<nav className="nav">
         <h1>Chagas</h1>
         <ul className='nav-links'>
+            <AuthButton></AuthButton>
             <li><Link to="/aulas"></Link>aulas</li>
             <li><Link to="/sobre"></Link>sobre</li>
         </ul>

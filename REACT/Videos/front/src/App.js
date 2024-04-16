a38555ef81404e81255388c70e416078d869b4b2
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-04-16 18:28:30 -0300

    feat: new app link

--- REACT/Videos/front/src/App.js
+++ REACT/Videos/front/src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
-
 function App() {
+
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/videos').then(res => res.json()).then(data => {
+      setVideos(data);
+    })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
+
+        {videos.map((id, index) => {
+          return (<a key={index} hre={`https://youtube.com/watch?v=${id}`}>
+              <img src={`https://youtube.com/vi=${id}/0.jpg`}></img>
+            </a>)
+        })}
       
       </header>
     </div>
